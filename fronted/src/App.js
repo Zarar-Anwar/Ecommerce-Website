e2f76d4831b04fed70c18db8a8d867cb30960be4
zZaala <zararanwar1234321@gmail.com>
2023-01-02 15:13:24 +0500

    2nd Commit

--- fronted/src/App.js
+++ fronted/src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import data from './data';
 
 function App() {
   return (
@@ -6,7 +7,26 @@ function App() {
       <header>
       <a href="/">amazona</a>  
       </header>
-      <main>list products</main>
+      <main>
+
+      <h1>Featured Products</h1>
+     <div className="products">
+      {
+        data.product.map(product=>(
+          <div key={product.slug}  className="product">
+          <a href={`/product/${product.slug}`} >
+          <img src={product.image} alt={product.name}/>
+          </a>
+          <div className='product-info'>
+          <a href={`/product/${product.slug}`}><p>{product.name}</p>
+          </a>
+          <strong><p>${product.prices}</p></strong>
+          <button>Add to cart</button>
+          </div>
+          </div>))
+      }
+      </div>
+      </main>
     </div>
   )
 }
