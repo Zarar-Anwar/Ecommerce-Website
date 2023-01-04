6a24871dfd84f60bea91713d9f382823d1efc952
zZaala <zararanwar1234321@gmail.com>
2023-01-04 18:32:44 +0500

    5th Commit

--- fronted/src/screens/HomeScreen.js
+++ fronted/src/screens/HomeScreen.js
@@ -1,13 +1,36 @@
 import { Link } from 'react-router-dom';
 import axios from 'axios'
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 
+const reducer=(state,action)=>{
+   switch(action.type){
+    case "FETCH_REQUEST":
+      return{...state,loading:true}
+     case "FETCH_SUCCES":
+      return{...state,loading:false,products:action.payload}
+      case "FETCH_FAILED":
+        return{...state,loading:false,error:action.payload}   
+    }
+}
+
+initialState={
+  products:[],
+  loading:true,
+  error:''
+}
 function HomeScreen() {
-    const [products,setProduct]=useState([])
+  const [{loading,error,products},dispatch]=useReducer(state,initialState)  
+  // const [products,setProduct]=useState([])
     useEffect(()=>{
      const fetchData=async()=>{
+      dispatch({type:"FETCH_REQUEST"})
+      try {
         const result=await axios.get('/api/products')
-        setProduct(result.data.product)
+        dispatch({type:"FETCH_SUCCES",payload:result.data})
+      } catch (error) {
+        dispatch({type:"FETCH_FAILED",payload:error.message})
+      }
+        // setProduct(result.data.product)
      }
      fetchData()
     },[])
@@ -16,6 +39,7 @@ function HomeScreen() {
       <h1>Featured Products</h1>
      <div className="products">
       {
+        loading?<div>...loading</div>:error?<div>{error}</div>:(
         products.map((product)=>(
           <div key={product.slug}  className="product">
           <Link to={`/product/${product.slug}`} >
@@ -27,7 +51,7 @@ function HomeScreen() {
           <strong><p>${product.prices}</p></strong>
           <button>Add to cart</button>
           </div>
-          </div>))
+          </div>)))
       }
       </div>
     </div>
