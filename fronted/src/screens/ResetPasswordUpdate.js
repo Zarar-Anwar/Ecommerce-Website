ee09f426696b74050795610a9165f0dba32a5d98
zZaala <zararanwar1234321@gmail.com>
2023-01-21 19:00:56 +0500

    commit

---
+++ fronted/src/screens/ResetPasswordUpdate.js
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+
+export default function ResetPasswordUpdate() {
+  return (
+    <>
+      <Grid container justifyContent='center'>
+        <Box component='form' id='resetpasswordupdate-Id' width="800px" textAlign='center'>
+            <h1>Update Password</h1>
+            
+        </Box>
+      </Grid>
+    </>
+  )
+}
